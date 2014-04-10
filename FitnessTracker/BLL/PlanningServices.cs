f20c3ac7ccfc36b47a7d22f131f43b78fd4dfd80
barrattac@github.com <barrattac@hotmail.com>
2014-04-09 23:17:25 -0400

    Workout Planner Page Started

---
+++ FitnessTracker/BLL/PlanningServices.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class PlanningServices
+    {
+        public DateTime GetFirstDay()
+        {
+            DateTime sun = DateTime.Now;
+            sun = sun.AddDays(-Convert.ToInt32(sun.DayOfWeek));
+            while (sun.Month == DateTime.Now.Month && sun.Day != 1)
+            {
+                sun = sun.AddDays(-7);
+            }
+            return sun;
+        }
+    }
+}
--- FitnessTracker/FitnessTracker/Controllers/WorkoutPlannerController.cs
+++ FitnessTracker/FitnessTracker/Controllers/WorkoutPlannerController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BLL;
 
 namespace FitnessTracker.Controllers
 {
-    public class HomeController : Controller
+    public class WorkoutPlannerController : Controller
     {
         public ActionResult Index()
         {
@@ -14,7 +15,8 @@ namespace FitnessTracker.Controllers
             {
                 return RedirectToAction("Register", "Account", new { });
             }
-            return View();
+            PlanningServices log = new PlanningServices();
+            return View(log.GetFirstDay());
         }
 
     }
